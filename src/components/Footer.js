9c286d2aa5614204a4560c1768fe16039b8563c7
Vlad-codeWork <94455918+Vlad-codeWork@users.noreply.github.com>
2021-11-19 03:39:42 +0200

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>About</a>
+        </footer>
+    )
+}
+
+export default Footer
